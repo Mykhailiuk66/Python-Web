d037e9b7fe4b789ebee837fac32825c40d8f252d
Vitaliy <mykhaylyuk66@gmail.com>
2022-12-21 18:26:41 +0200

    -

--- lab15/run.py
+++ lab15/run.py
@@ -1,6 +1,6 @@
 from os import environ
 from app import create_app
 
-if __name__ == "__main__":
-    app = create_app(environ.get('FLASK_CONFIG'))
-    app.run()
+# if __name__ == "__main__":
+app = create_app(environ.get('FLASK_CONFIG'))
+# app.run()
